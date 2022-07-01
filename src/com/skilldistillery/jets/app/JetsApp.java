69f7a7f48b8e8d4f7364853af47d3f96e000ae42
tblandin <tiarra.blandin@outlook.com>
2022-07-01 16:00:16 -0600

    initial commit

---
+++ src/com/skilldistillery/jets/app/JetsApp.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.jets.app;
+
+public class JetsApp {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/com/skilldistillery/jets/entities/AirField.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.jets.entities;
+
+import java.util.List;
+
+public class AirField {
+	private List<Jet> fleet;
+
+}
